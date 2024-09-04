7b58735f699c0939264e10a3a9d87990716fe741
heizetagram <davidlu1999@gmail.com>
2024-09-04 20:46:27 +0200

    Refactor

--- src/main/java/com/example/_2024_09_06unicodejpaopgave/config/InitData.java
+++ src/main/java/com/example/_2024_09_06unicodejpaopgave/config/InitData.java
@@ -1,55 +1,17 @@
 package com.example._2024_09_06unicodejpaopgave.config;
 
-import com.example._2024_09_06unicodejpaopgave.models.Unicode;
-import com.example._2024_09_06unicodejpaopgave.repositories.UnicodeRepository;
+import com.example._2024_09_06unicodejpaopgave.services.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Component
 public class InitData implements CommandLineRunner {
     @Autowired
-    private UnicodeRepository unicodeRepository;
+    private CharacterService characterService;
 
     @Override
     public void run(String... args) throws Exception {
-        saveUnicodeToDB();
-    }
-
-    // Save unicodes to database
-    private void saveUnicodeToDB() {
-        Set<Character> characterSet = new HashSet<>();
-        int n = 0;
-        char charValue = (char) n;
-        Unicode unicode = new Unicode();
-
-        while (characterSet.add(charValue)) {
-            unicode.setUnicode(n);
-            unicode.setCharValue(charValue);
-            unicode.setName(Character.getName(n));
-            unicode.setDescription(checkCharType(charValue));
-
-            unicodeRepository.save(unicode);
-            n++;
-            charValue = (char) n;
-        }
-    }
-
-    // Check character type
-    private String checkCharType(Character c) {
-        String charType;
-        if (Character.isLetter(c)) {
-            charType = "Letter";
-        } else if (Character.isDigit(c)) {
-            charType = "Digit";
-        } else if (Character.isEmoji(c)) {
-            charType = "Emoji";
-        } else {
-            charType = null;
-        }
-        return charType;
+        characterService.saveUnicodeToDB();
     }
 }
--- src/main/java/com/example/_2024_09_06unicodejpaopgave/services/CharacterService.java
+++ src/main/java/com/example/_2024_09_06unicodejpaopgave/services/CharacterService.java
@@ -1,12 +1,19 @@
 package com.example._2024_09_06unicodejpaopgave.services;
 
+import com.example._2024_09_06unicodejpaopgave.models.Unicode;
+import com.example._2024_09_06unicodejpaopgave.repositories.UnicodeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CharacterService {
+    @Autowired
+    private UnicodeRepository unicodeRepository;
 
     // Get a list of characters by entered amount
     public List<Character> getCharactersByAmount(char c, int i) {
@@ -15,5 +22,39 @@ public class CharacterService {
             characterList.add(c);
         }
         return characterList;
+    }
+
+    // Save unicodes to database
+    public void saveUnicodeToDB() {
+        Set<Character> characterSet = new HashSet<>();
+        int n = 0;
+        char charValue = (char) n;
+        Unicode unicode = new Unicode();
+
+        while (characterSet.add(charValue)) {
+            unicode.setUnicode(n);
+            unicode.setCharValue(charValue);
+            unicode.setName(Character.getName(n));
+            unicode.setDescription(checkCharType(charValue));
+
+            unicodeRepository.save(unicode);
+            n++;
+            charValue = (char) n;
+        }
+    }
+
+    // Check character type
+    private String checkCharType(Character c) {
+        String charType;
+        if (Character.isLetter(c)) {
+            charType = "Letter";
+        } else if (Character.isDigit(c)) {
+            charType = "Digit";
+        } else if (Character.isEmoji(c)) {
+            charType = "Emoji";
+        } else {
+            charType = null;
+        }
+        return charType;
     }
 }
